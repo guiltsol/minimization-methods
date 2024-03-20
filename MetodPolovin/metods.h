00f017991b7422376b9ddb9229794a44c8f038d5
guiltsol <maksimgusev123123@mail.ru>
2024-03-20 20:00:26 +0300

    fix method

--- MetodPolovin/metods.h
+++ MetodPolovin/metods.h
@@ -66,18 +66,19 @@ void met_poisk(double a, double b, double eps) {
 	double x2 = x1 + h;
 	int i = 0;
 	while (abs(h) > eps) {
+		i++;
 		if (fun(x1) > fun(x2)) {
 			x1 = x2;
 			if (x1 > a && x1 < b) {
 				x2 += h;
 			}
-			i++;
+			
 		}
 		else {
 			x1 = x2;
 			h /= -4;
 			x2 += h;
-			i++;
+			
 		}
 	}
 	cout << "Xmin = " << x2 << " F(Xmin) = " << fun(x2)<<"\n";
@@ -88,21 +89,30 @@ void met_sechen(double a, double b, double eps) {
 	double tao = (sqrt(5) - 1) / 2;
 	double x1 = a + (1 - tao) * (b - a);
 	double x2 = a + tao * (b - a);
+	double fx1 = fun(x1);
+	double fx2 = fun(x2);
+	int i = 1;
 	while (abs(b - a) > eps) {
-		if (fun(x1) > fun(x2)) {
+		if (fx1> fx2) {
 			a = x1;
 			x1 = x2;
 			x2 = a + tao * (b - a);
+			fx1 = fx2;
+			fx2 = fun(x2);
+			i++;
 		}
 		else {
 			b = x2;
 			x2 = x1;
 			x1 = a + (1 - tao) * (b - a);
-
+			fx2 = fx1;
+			fx1 = fun(x1);
+			i++;
 		}
 	}
 	double min = (b + a )/ 2;
 	cout << "Xmin = " << min << " F(Xmin) = " << fun(min)<<"\n";
+	cout << "I = " << i << "\n";
 
 }
 
@@ -110,17 +120,21 @@ void met_middot(double a, double b, double eps) {
 	double first = a;
 	double sec = b;
 	double x = (first+sec) / 2;
+	int i = 1;
 	while (abs(sec - first) > eps) {
 		if (fun_pr1(x) > 0) {
 			sec = x;
 			x = (first + sec) / 2;
+			i++;
 		}
 		else {
 			first = x;
 			x = (first + sec) / 2;
+			i++;
 		}
 	}
 	cout << "Xmin = " << x << " F(Xmin) = " << fun(x) << "\n";
+	cout << "i = " << i << "\n";
 
 
 }
@@ -129,16 +143,19 @@ void met_hord(double a, double b, double eps) {
 	double first = a;
 	double sec = b;
 	double x_min = 0;
+	int i = 1;
 	if (fun_pr1(first) * fun_pr1(sec) < 0) {
 		x_min = first - (fun_pr1(first)) / (fun_pr1(sec) - fun_pr1(first)) * (sec - first);
 		while (fun_pr1(x_min) > eps) {
 			if (fun_pr1(x_min) > 0) { 
 				sec = x_min; 
 				x_min = first - (fun_pr1(first)) / (fun_pr1(sec) - fun_pr1(first)) * (sec - first);
+				i++;
 			}
 			else { 
 				first = x_min;
 				x_min = first - (fun_pr1(first)) / (fun_pr1(sec) - fun_pr1(first)) * (sec - first);
+				i++;
 			}
 		}
 	
@@ -148,6 +165,7 @@ void met_hord(double a, double b, double eps) {
 		if ((fun_pr1(first) < 0 && fun_pr1(sec) < 0) || (fun_pr1(sec) == 0)) { x_min = sec; }
 	}
 	cout << "Xmin = " << x_min << " F(Xmin) = " << fun(x_min) << "\n";
+	cout << "i = " << i << "\n";
 
 
 }
@@ -155,12 +173,16 @@ void met_hord(double a, double b, double eps) {
 void met_nyton(double a, double b, double eps) {
 	double x0 = -12;
 	double x1 = x0 - (fun_pr1(x0)) / (fun_pr2(x0));
+	int i = 1;
 	while (fun_pr1(x1) > eps) {
 		x0 = x1;
 		x1 = x0 - (fun_pr1(x0)) / (fun_pr2(x0));
+		i++;
 	}
 	cout << "Xmin = " << x1 << " F(Xmin) = " << fun(x1) << "\n";
+	cout << "i = " << i << "\n";
 
 
 }
+
 
